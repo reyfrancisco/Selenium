c3ac8293c1abfdb433de862526aa3e06f6a30fd9
REY.MORALES <REY.MORALES@MXTORL1T4MK63.zurich.com>
2021-10-20 15:36:15 -0500

    update to add explicit and implicit wait

--- Selenium/src/test/java/com/google/GoogleTest.java
+++ Selenium/src/test/java/com/google/GoogleTest.java
@@ -2,11 +2,32 @@ package com.google;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 
 public class GoogleTest {
   @Test
   public void f() {
+	  System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chrome/chromedriver.exe");
+	  WebDriver driver = new ChromeDriver(); 	 //  This object can change form like polymorphism behavior  
+//	  driver.get("https://www.zurich.com/");
+	  driver.get("https://www.google.com/");
+	  driver.manage().window().maximize();	  
+	  
+	  //Implicit Wait
+	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	  
+	  WebDriverWait wait = new WebDriverWait(driver, 15);
+	  wait.until(ExpectedConditions.titleContains("Google"));
+	  
+	  System.out.println("AVANZO SCRIPT");	  
+	  
   }
   @BeforeTest
   public void beforeTest() {
